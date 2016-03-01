d4d5fd9eba65bd910960524da235f92a8ad64755
Paul Ketelaars <ketelaars@gmail.com>
2016-03-01 09:57:34 +0100

    Removed Jquery and template since not needed

--- src/Skycons/widget/Skycons.js
+++ src/Skycons/widget/Skycons.js
@@ -17,33 +17,28 @@
 
 // Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
 define([
-    "dojo/_base/declare",
-    "mxui/widget/_WidgetBase",
-
-    "mxui/dom",
-    "dojo/dom",
-    "dojo/dom-class",
-    "dojo/dom-style",
-    "dojo/dom-construct",
-    "dojo/text",
-    "dojo/html",
-
-    "Skycons/lib/skycons"
-], function(declare, _WidgetBase, dom, dojoDom,   dojoClass, dojoStyle, dojoConstruct,  dojoText, dojoHtml, Skycons) {
-    "use strict";
+  'dojo/_base/declare',
+  'mxui/widget/_WidgetBase',
+  'mxui/dom',
+  'dojo/dom',
+  'dojo/dom-class',
+  'dojo/dom-style',
+  'dojo/dom-construct',
+  'dojo/text',
+  'dojo/html',
+  'Skycons/lib/skycons'
+ ], function (declare, _WidgetBase, dom, dojoDom, dojoClass, dojoStyle, dojoConstruct, dojoText, dojoHtml, Skycons) {
+    'use strict';
 
     // Declare widget's prototype.
-    return declare("Skycons.widget.Skycons", [ _WidgetBase ], {
+    return declare('Skycons.widget.Skycons', [ _WidgetBase ], {
 
         // Parameters configured in the Modeler.
-        mfToExecute: "",
-        messageString: "",
-        backgroundColor: "",
-        icon: "",
-        width: "",
-        height: "",
-        animated: "",
-        color: "",
+        icon: '',
+        width: '',
+        height: '',
+        animated: '',
+        color: '',
 
         // Internal variables
         _skyNode: null,
@@ -51,44 +46,45 @@ define([
         // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
         postCreate: function() {
           //logger.level(logger.DEBUG);
-          logger.debug(this.id + ".postCreate");
+          logger.debug(this.id + '.postCreate');
         },
 
         update: function (obj, callback) {
-          logger.debug(this.id + ".update");
+          logger.debug(this.id + '.update');
           if (this._skyNode === null) {
             // callback is called when the widget is ready (the domNode is created)
             this._createSkycon(callback);
           } else {
+            domConstruct.destroy(this._skyNode);
             callback();
           }
         },
 
         _createSkycon: function (callback) {
-            logger.debug(this.id + "._createSkycon create domNode");
+            logger.debug(this.id + '._createSkycon create domNode');
 
-            this._skyNode = dojoConstruct.create("canvas", {
+            this._skyNode = dojoConstruct.create('canvas', {
                 class: this.icon,
                 width: this.width,
                 height: this.height
-            }, this.domNode, "only");
+            }, this.domNode, 'only');
 
-            logger.debug(this.id + "._createSkycon start");
+            logger.debug(this.id + '._createSkycon start');
             //Create a list with all the possible values
             var list = [
-              "clear_day", "clear_night", "partly_cloudy_day",
-              "partly_cloudy_night", "cloudy", "rain", "sleet", "snow", "wind",
-              "fog"],
+              'clear_day', 'clear_night', 'partly_cloudy_day',
+              'partly_cloudy_night', 'cloudy', 'rain', 'sleet', 'snow', 'wind',
+              'fog'],
               //i,
               skycons;
 
-            if (this.coloring === "one"){
+            if (this.coloring === 'one'){
               var colorString = this.color;
-              skycons = new Skycons({"color": colorString});
+              skycons = new Skycons({'color': colorString});
             } else {
-              logger.debug(this.id + "._createSkycon colored");
+              logger.debug(this.id + '._createSkycon colored');
               // Colored instance of Skycon
-              skycons = new Skycons({"monochrome": false});
+              skycons = new Skycons({'monochrome': false});
             }
 
             for (var i = list.length; i>=0; i--) {
@@ -100,18 +96,17 @@ define([
                 }
             }
 
-            if (this.animated === true){
+            if (this.animated === true) {
                 skycons.play();
             }
 
-            if (typeof callback === "function") {
+            if (typeof callback === 'function') {
               callback();
             }
         }
-
     });
 });
 
-require(["Skycons/widget/Skycons"], function() {
-    "use strict";
+require(['Skycons/widget/Skycons'], function() {
+    'use strict';
 });
